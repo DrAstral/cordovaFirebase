781c29d988069fa638221fa1f6dd7db8742a9936
DrAstral <91513012+DrAstral@users.noreply.github.com>
2021-10-01 21:27:02 -0300

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyA66y1AesDTDTN1NOpba4BZjk1wCjeq2Z8",
+  authDomain: "testeaula-5fc6d.firebaseapp.com",
+  projectId: "testeaula-5fc6d",
+  storageBucket: "testeaula-5fc6d.appspot.com",
+  messagingSenderId: "148100133291",
+  appId: "1:148100133291:web:fc365d43f8ca81fe4ca2ee",
+  measurementId: "G-7FTX9KRXJ7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
